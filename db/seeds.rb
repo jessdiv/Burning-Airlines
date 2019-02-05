8a88d51284551d9fe4be16353c9358931ed152ca
jessdiv <jessica.diver@hotmail.com>
2019-02-05 17:26:06 +1100

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+u1 = User.create :username => 'jessdiv', :email => 'jess@ga.com', :password => 'chicken', :admin => true
+u2 = User.create :username => 'sarita', :email => 'sarita@ga.co', :password => 'chicken', :admin => false
+
+Airplane.destroy_all
+a1 = Airplane.create :total_seats => 10
+
+Flight.destroy_all
+f1 = Flight.create :origin => 'Sydney', :destination => 'Melbourne'
+
+# Associations ####
+
+f1.airplane << a1
