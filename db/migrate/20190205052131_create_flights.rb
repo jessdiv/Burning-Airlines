1e830c29e1c6ae602a4d639c837baa7a79408ccf
jessdiv <jessica.diver@hotmail.com>
2019-02-05 16:24:24 +1100

    created rails app

---
+++ db/migrate/20190205052131_create_flights.rb
@@ -0,0 +1,12 @@
+class CreateFlights < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flights do |t|
+      t.text :origin
+      t.text :destination
+      t.date :date
+      t.integer :airplane_id
+
+      t.timestamps
+    end
+  end
+end
